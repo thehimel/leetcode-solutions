7ed39072a3285578c0fa1238a83200dfee2dfe49
Himel Das <60639301+thehimel@users.noreply.github.com>
2023-12-08 20:58:56 +0100

    Updated two sum

--- 1_two_sum/two_sum.py
+++ 1_two_sum/two_sum.py
@@ -5,7 +5,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = dict()
         for i, num in enumerate(nums):
-            difference = target - num
-            if difference in seen:
-                return [i, seen[difference]]
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
             seen[num] = i
